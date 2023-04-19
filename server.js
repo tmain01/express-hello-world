c407996f2fab5b182068165db75b6661aaaaaa56
tmain01 <129995459+tmain01@users.noreply.github.com>
2023-04-19 15:54:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const app = require('./public/app.js')
 
 const port = process.env.PORT || 3000
 
-app.set('view engine', 'ejs');
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })

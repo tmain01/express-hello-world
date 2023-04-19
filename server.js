fd41b4de99a35e00f96748d4565912c6bfff14c0
tmain01 <129995459+tmain01@users.noreply.github.com>
2023-04-19 15:10:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require('./public/app.js')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const port = process.env.PORT || 3000
+
+global.document = new JSDOM("https://asmoverlord.cyclic.app").window.document;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
